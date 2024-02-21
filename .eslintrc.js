efc9c920b58d8b371dfd6f5e7b27e5cd7ded7721
Yuri Andrade <48354468+yandrade11@users.noreply.github.com>
2024-02-21 18:16:09 -0300

    primeiro commit
    trazendo da aula

---
+++ .eslintrc.js
@@ -0,0 +1,37 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:react/recommended",
+    'plugin:react-hooks/recommended',
+    // 'airbnb',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parser: '@babel/eslint-parser',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  plugins: ['react', "react-hooks"],
+  settings: {
+    react: {
+      version: "detect",
+    }
+  },
+  rules: {
+    "react/react-in-jsx-scope": "off",
+    'react/jsx-filename-extension': 0,
+    'react/state-in-constructor': 0,
+    'react/forbid-prop-types': 0,
+  },
+};
